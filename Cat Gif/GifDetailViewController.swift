d1040a9a75dcb13191293c01da3a990a1c97f480
Mohcine Belarrem <belarrem.mohcine@gmail.com>
2019-05-31 19:15:45 +0200

    Renders text in details view as html

--- Cat Gif/GifDetailViewController.swift
+++ Cat Gif/GifDetailViewController.swift
@@ -20,7 +20,17 @@ class GifDetailViewController: UIViewController {
         super.viewDidLoad()
         
         navigationItem.title = gif.id
-        textView.text = text
+        
+        let fullPage = "<!DOCTYPE html> <html> <head> <style> p{font-size: 20px;; text-align: justify;} </style> </head> <h1>\(gif.id)</h1> <body>"+text+"</body></html>"
+        
+        let htmlData = NSString(string: fullPage).data(using: String.Encoding.utf8.rawValue)
+        let options = [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html]
+        
+        let attributedString = try! NSAttributedString(data: htmlData!,
+                                                       options: options,
+                                                       documentAttributes: nil)
+        
+        textView.attributedText = attributedString
         
         DispatchQueue.global().async {
             
@@ -37,9 +47,9 @@ class GifDetailViewController: UIViewController {
         
     }
     
-    
     override func viewDidAppear(_ animated: Bool) {
         self.textView.flashScrollIndicators()
+        self.textView.setContentOffset(.zero, animated: true)
     }
     
 }
