04108e1136545dd75dbe6f465eef70ccff9df85e
Mohcine Belarrem <belarrem.mohcine@gmail.com>
2019-05-31 14:24:10 +0200

    Adds Gif Manager which the class responsible for fetching PhotosData

---
+++ Cat Gif/GifManager.swift
@@ -0,0 +1,71 @@
+//
+//  GifManager.swift
+//  Cat Gif
+//
+//  Created by Mohcine Belarrem on 31/05/2019.
+//  Copyright © 2019 mohcine. All rights reserved.
+//
+
+import Foundation
+
+
+///This will be our base struct, following the JSon Pattern
+struct CatGif : Decodable {
+    let id : String
+    let url : String
+    let source_url : String
+    
+}
+
+final class GifManager {
+    private init() {}
+    static let shared = GifManager()
+    
+    private var gifsList = [CatGif]()
+    
+    func getGifsData(numberOfGifs : Int) {
+        
+        if !gifsList.isEmpty {
+            gifsList.removeAll(keepingCapacity: false)
+        }
+        
+        let stringUrl = "https://api.thecatapi.com/api/images/get?format=json&type=gif"
+        guard let Url = URL(string: stringUrl) else {
+            return
+        }
+        
+        
+        for _ in 1...numberOfGifs {
+        
+        URLSession.shared.dataTask(with: Url) {[weak self] (data,response,error) in
+            
+            do {
+                
+                   let gifArray  = try JSONDecoder().decode([CatGif].self, from: data ?? Data())
+                    
+                    self?.gifsList.append(gifArray.first!)
+                    
+                    let notification = Notification(name: Notification.Name("dataReady"))
+                    
+                    NotificationCenter.default.post(notification)
+            
+            } catch {
+                print("Something Went wrong while parsing data.")
+            }
+            
+            }.resume()
+            
+        }
+    }
+    
+    
+    func printList() {
+        
+        for gif in gifsList {
+            
+            print("\(gif.id) \(gif.url)")
+            
+        }
+    }
+    
+}
