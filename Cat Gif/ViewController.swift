7a525a0f81eeaf59c115e244a30d4ade16ae4d3c
Mohcine Belarrem <belarrem.mohcine@gmail.com>
2019-05-31 13:43:53 +0200

    Initial Commit

---
+++ Cat Gif/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cat Gif
+//
+//  Created by Mohcine Belarrem on 31/05/2019.
+//  Copyright © 2019 mohcine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
